7881bb509a4316b38829ae8c5cb1e60278b8e4c6
codynoskov <k.s.noskov@gmail.com>
2026-01-10 20:26:56 +0100

    Configure GitHub Pages deployment with GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
+  output: 'static',
+  base: '/HCT/',
   devToolbar: {
     enabled: false
   }
